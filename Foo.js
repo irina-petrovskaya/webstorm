56ff01365c89f2230bf75c1c026b20dabdb9f289
ubuntu user <ubuntu@foo.com>
2012-11-27 21:15:24 +0400

    First commit

---
+++ Foo.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: user
+ * Date: 11/27/12
+ * Time: 9:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function aaa(){
+    return 0;
+}
